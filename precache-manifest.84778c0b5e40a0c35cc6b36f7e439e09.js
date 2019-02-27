ec135adb441124eef0c0c968f71f972b877c6e6d
denisyuk26 <sergey.denisyuk@gmail.com>
2019-02-27 14:29:23 +0200

    Updates

---
+++ precache-manifest.84778c0b5e40a0c35cc6b36f7e439e09.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3deaedc0291104947ef7",
+    "url": "/github_top_ten/static/js/runtime~main.3deaedc0.js"
+  },
+  {
+    "revision": "80ca247e5d3e72a6cd01",
+    "url": "/github_top_ten/static/js/main.80ca247e.chunk.js"
+  },
+  {
+    "revision": "a5fb218d7619b8837123",
+    "url": "/github_top_ten/static/js/2.a5fb218d.chunk.js"
+  },
+  {
+    "revision": "81facf99f22738eabcef849d0e4cf2b9",
+    "url": "/github_top_ten/index.html"
+  }
+];
